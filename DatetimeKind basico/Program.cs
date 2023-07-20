7933cdbb5ed73a381c0b2e0a9962185cf77484ed
lucalmr <lucalmr12@gmail.com>
2023-07-20 19:04:11 -0300

    Adicionar arquivos de projeto.

---
+++ DatetimeKind basico/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatetimeKind_basico {
+    internal class Program {
+        static void Main(string[] args) {
+            //1º data sistema local /2 º UTC fuso horario Greenwich 3 horas atrasado (Londres) mean time//3º Unspecified
+            //armazenar em UTC (universal)
+            //porem na hora de mostrar para o usuario e necessario instancia em formato local 
+            //consequentemente o usuario ira verificar o horario local ( de sua regiao)
+            DateTime Datalocal = new DateTime(2000, 08, 11,20,30,55, DateTimeKind.Local);
+            DateTime D2 = new DateTime(2000, 08, 11, 20, 30, 55, DateTimeKind.Utc);
+            DateTime D3 = new DateTime(2000, 08, 11, 20, 30, 55);
+
+            Console.WriteLine("Data Local: " +Datalocal);
+            Console.WriteLine("Data Kind : " + Datalocal.Kind) ;
+            Console.WriteLine("Data to local : " +Datalocal.ToLocalTime());
+            Console.WriteLine("DataLocal to UTC: 3 HORAS (Londres) " + Datalocal.ToUniversalTime());
+            Console.WriteLine() ;
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Data Local: " + D2);
+            Console.WriteLine("Data Kind : " + D2.Kind);
+            Console.WriteLine("Data to local volta 3 horas green Londres : " + D2.ToLocalTime());
+            Console.WriteLine("DataLocal to UTC: 3 HORAS (Londres) " + D2.ToUniversalTime());
+            
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("---basicamente não se sabe quanto e utc ou local a hora-----------");
+            //utlizando o metodo universal  Unspecified
+            Console.WriteLine("Data Local: " + D3);
+            Console.WriteLine("Data Kind : " + D3.Kind);
+            Console.WriteLine("Data to local 3 horas menos : " + D3.ToLocalTime());
+            Console.WriteLine("DataLocal to UTC: 3 HORAS (Londres) " + D3.ToUniversalTime());
+
+
+        }
+    }
+}
---
+++ DatetimeKind basico/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("DatetimeKind basico")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DatetimeKind basico")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("596160f1-e83e-461c-b586-7013f0c301e0")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
